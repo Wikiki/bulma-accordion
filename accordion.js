a505bbdeb646a3e373e501c6371f5546f565463d
Wikiki <wikiki@protonmail.com>
2017-11-21 13:49:19 +0100

    Fix animation

--- accordion.js
+++ accordion.js
@@ -1,14 +1,14 @@
-let accordions = document.querySelectorAll('.accordion');
+let accordions = document.querySelectorAll('.accordions');
 if (accordions) {
   accordions.forEach(accordion => {
-    let items = accordion.querySelectorAll('.message');
+    let items = accordion.querySelectorAll('.accordion');
     if (items) {
       items.forEach(item => {
         item.querySelector('.toggle').addEventListener('click', e => {
           e.preventDefault();
           let currentItem = e.target.parentNode.parentNode;
           if (!currentItem.classList.contains('is-active')) {
-            let activeItem = accordion.querySelector('.message.is-active');
+            let activeItem = accordion.querySelector('.accordion.is-active');
             if (activeItem) {
               activeItem.classList.remove('is-active');
             }
