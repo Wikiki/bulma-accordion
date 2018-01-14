e92da2416d64b3845500240321fb41278ec2c88b
Wikiki <wikiki@protonmail.com>
2018-01-14 12:13:16 +0100

    Add TouchEvent management

--- accordion.js
+++ accordion.js
@@ -1,20 +1,23 @@
+const MOUSE_EVENTS = ['click', 'touchstart'];
+
 document.addEventListener( 'DOMContentLoaded', function () {
   var accordions = document.querySelectorAll('.accordions');
   [].forEach.call(accordions, function(accordion) {
     var items = accordion.querySelectorAll('.accordion');
     [].forEach.call(items, function(item) {
-      item.querySelector('.toggle').addEventListener('click', e => {
-        e.preventDefault();
-        let currentItem = e.target.parentNode.parentNode;
-        if (!currentItem.classList.contains('is-active')) {
-          let activeItem = accordion.querySelector('.accordion.is-active');
-          if (activeItem) {
-            activeItem.classList.remove('is-active');
+      MOUSE_EVENTS.forEach((event) => {
+        item.querySelector('.toggle').addEventListener(event, e => {
+          e.preventDefault();
+          if (!item.classList.contains('is-active')) {
+            let activeItem = accordion.querySelector('.accordion.is-active');
+            if (activeItem) {
+              activeItem.classList.remove('is-active');
+            }
+            item.classList.add('is-active');
+          } else {
+            item.classList.remove('is-active');
           }
-          currentItem.classList.add('is-active');
-        } else {
-          currentItem.classList.remove('is-active');
-        }
+        });
       });
     });
   });
